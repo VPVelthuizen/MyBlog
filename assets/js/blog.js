f65edaab042b3f27213b418778dcbca965cb6233
VPVelthuizen <vpvelthuizen@gmail.com>
2024-07-01 16:46:38 -0400

    added the light/dark button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,18 @@
-// TO-DO Add button controls for light/dark mode
+const toggleBtn = document.getElementById('toggle');
 
-// TO-DO Back button that takes us back to landing page for more blog entries
+// TO-DO Add button controls for light/dark mode
+toggleBtn.addEventListener('click', function () {
+    let toggle = JSON.parse(localStorage.getItem('toggle'))
+    if (toggle == "1") {
+        localStorage.setItem('toggle', "0");
+        body.classList.add('body-dark');
+        header.classList.add('footer')
+        footer.classList.remove('footer')
+    }
+    else {
+        localStorage.setItem('toggle', "1");
+        body.classList.remove('body-dark');
+        header.classList.remove('footer');
+        footer.classList.add('footer')
+    };
+})
